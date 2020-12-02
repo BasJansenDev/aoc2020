dcbf4c18692581ac60ec42084801e20222147296
Bas Jansen <b.jansen@student.utwente.nl>
2020-12-02 10:21:19 +0100

    removed unnecessary condition

--- day2/part2.py
+++ day2/part2.py
@@ -4,7 +4,7 @@ def main():
     count = 0
     for line in inp:
         sp = re.split(' |-|:',line)
-        if(len(sp[4]) >= int(sp[1]) and ((sp[2] == sp[4][int(sp[0])-1]) ^ (sp[2] == sp[4][int(sp[1])-1]))):
+        if((sp[2] == sp[4][int(sp[0])-1]) ^ (sp[2] == sp[4][int(sp[1])-1])):
             count += 1
 
     print(count)
