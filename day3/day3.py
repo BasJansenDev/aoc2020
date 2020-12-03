e55762f7d78c99599a25ebff300b2c9c87626321
Bas Jansen <b.jansen@student.utwente.nl>
2020-12-03 12:35:01 +0100

    Oneliner added

--- day3/day3.py
+++ day3/day3.py
@@ -9,9 +9,14 @@ def main(right,down):
         pos = (pos + right) % len(inp[i])
     return count
 
+def oneliner(data,right,down):
+    return len(list(filter(lambda v : data[0::down][v][(v*right) % len(data[0])] == "#",[i for i in range(0,round(len(data)/down))])))
+
 def inputAsList():
     f = open('input')
     return f.read().splitlines()
 
-print('Part 1 : ' + str(main(3,1)))
+print('Part 1 : ' + str(main(3,2)))
+print('Part 1 : ' + str(oneliner(inputAsList(),3,2)))
 print('Part 2 : ' + str(main(1,1) * main(3,1) * main(5,1) * main(7,1) * main(1,2)))
+print('Part 2 : ' + str(oneliner(inputAsList(),1,1) * oneliner(inputAsList(),3,1) * oneliner(inputAsList(),5,1) * oneliner(inputAsList(),7,1) * oneliner(inputAsList(),1,2)))
