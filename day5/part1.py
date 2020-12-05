0e8064189b7acf2f351f86a3bdc9d9d6eb646778
Bas Jansen <b.jansen@student.utwente.nl>
2020-12-05 17:07:25 +0100

    Implemented oneliners

--- day5/part1.py
+++ day5/part1.py
@@ -20,10 +20,12 @@ def main():
         mx = max(mx,(check(0,127,re.sub('[LR]','',i)) * 8 + check(0,7,re.sub('[FB]','',i))))
     return mx
 
-
+def oneliner():
+    return max([int("".join(str(a) for a in b), 2) for b in [[ord(z) % 7 % 2 for z in y] for y in inputAsList()]])
 
 def inputAsList():
     f = open('input')
     return f.read().splitlines()
 
-print(main())
+print("Part 1: " + str(main()))
+print("Part 1: " + str(oneliner()))
--- day5/part2.py
+++ day5/part2.py
@@ -22,10 +22,13 @@ def main():
         lst += [(check(0, 127, re.sub('[LR]', '', i)) * 8 + check(0, 7, re.sub('[FB]', '', i)))]
     return list(set(lst) ^ set(list(range(32, max(lst)))))[0]
 
+def oneliner():
+    return list(set([int("".join(str(a) for a in b), 2) for b in [[ord(z) % 7 % 2 for z in y] for y in inputAsList()]]) ^ set(list(range(32, 848))))[0]
 
 def inputAsList():
     f = open('input')
     return f.read().splitlines()
 
 
-print(main())
+print("Part 2: " + str(main()))
+print("Part 2: " + str(oneliner()))
