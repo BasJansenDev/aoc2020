ffcb920d2f551e826475d99de3ac18d14bab9215
Bas Jansen <b.jansen@student.utwente.nl>
2020-12-06 11:39:48 +0100

    Merged both parts into one.

--- day6/day6.py
+++ day6/day6.py
@@ -1,10 +1,11 @@
-def main():
+def part1():
+    return sum([len(set(i.replace('\n',''))) for i in inputAsList()])
+def part2():
     return sum([len(set.intersection(*map(set,i.split('\n')))) for i in inputAsList()])
-
 
 def inputAsList():
     f = open('input')
     return f.read().split('\n\n')
 
-
-print("Part 1: " + str(main()))
+print("Part 1: " + str(part1()))
+print("Part 2: " + str(part2()))
--- day6/part1.py
+++
@@ -1,9 +0,0 @@
-def main():
-    return sum([len(set(i.replace('\n',''))) for i in inputAsList()])
-
-def inputAsList():
-    f = open('input')
-    return f.read().split('\n\n')
-
-
-print("Part 1: " + str(main()))
