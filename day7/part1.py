db529c51b281dc47cd7b97aa9b7f07b01fbff892
Bas Jansen <b.jansen@student.utwente.nl>
2020-12-07 11:13:06 +0100

    Small cleanup

--- day7/part1.py
+++ day7/part1.py
@@ -1,23 +1,21 @@
 import re
-def dictSearch(key, dic, str,count):
+dic = {}
+def dictSearch(key, str,bool):
     if key == "nootherbag":
-        return 0
+        return False
     elif str in dic[key]:
-        return 1
+        return True
     else:
         for value in dic[key].split(','):
-            count += dictSearch(value.rstrip('s'),dic,str,count)
-        return count
+            bool |= dictSearch(value.rstrip('s'),str,bool)
+        return bool
 
 def main():
-    inp = inputAsList()
-    ls = [i.split('contain') for i in inp]
-    dic = {}
-    for j in ls:
+    for j in [i.split('contain') for i in inputAsList()]:
         dic[re.sub(r'[ .0-9]', '', j[0]).rstrip('s')] = re.sub(r'[ .0-9]', '', j[1])
     count = 0
     for key in dic.keys():
-        if(dictSearch(key, dic,'shinygoldbag',0) > 0):
+        if(dictSearch(key, 'shinygoldbag',False)):
             count += 1
     return count
 
--- day7/part2.py
+++ day7/part2.py
@@ -1,22 +1,19 @@
 import re
-def dictSearch(key, dic, count):
+dic = {}
+def dictSearch(key):
     lst = []
     for value in dic[key]:
         if(value[1]) == "nootherbags":
             lst.append(0)
         else:
-            val = dictSearch(value[1].rstrip('s'), dic, count)
+            val = dictSearch(value[1].rstrip('s'))
             lst.append(int(value[0]) + (int(value[0]) * val))
     return sum(lst)
 
-
 def main():
-    inp = inputAsList()
-    ls = [i.split('contain') for i in inp]
-    dic = {}
-    for j in ls:
+    for j in [i.split('contain') for i in inputAsList()]:
         dic[re.sub(r'[ .0-9]', '', j[0]).rstrip('s')] = [[re.sub(r'[ .a-z]', '', k),re.sub(r'[ .0-9]', '', k)] for k in j[1].split(',')]
-    return dictSearch('shinygoldbag', dic, 0)
+    return dictSearch('shinygoldbag')
 
 def inputAsList():
     f = open('input')
